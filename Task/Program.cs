3ab3fe59a2d4a6f552504d55cfb1b9518788d613
Irina Korovnikova <irina.shako19@gmail.com>
2024-11-24 20:05:30 +0500

    added program in Task

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] arr1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arr2 = new string[arr1.Length];
+
+void FindSpecialElements(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FindSpecialElements(arr1, arr2);
+PrintArray(arr2);
